05db64075996d6f161a43176407fb089ac350e6f
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2022-06-20 19:43:49 -0400

    Update MadLibsGame.php

--- PHP_tutorial/MadLibsGame.php
+++ PHP_tutorial/MadLibsGame.php
@@ -1,4 +1,11 @@
-<php?
+<html>
+    <body>
 
+        <?php
+            echo "Roses are red <br>";
+            echo "Violets are blue <br>";
+            echo "I love you <br>";
+        ?>
 
-?>
+    </body>
+</html>
