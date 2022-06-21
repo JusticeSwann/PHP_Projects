e7041b551fb243a490af7e55361675e123354e8d
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2022-06-21 13:45:52 -0400

    functions and return statements
    Completed the function and return statements from freecodecamp Giraffe Academy collab video.

---
+++ PHP_tutorial/functions.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+
+        
+        function sayHi($name, $age){
+            echo "Hello $name. You're $age years old <br>";
+        }
+
+        sayHi("Justice",40); 
+        sayHi("Camila", 35);
+    ?>
+
+</body>
+</html>
