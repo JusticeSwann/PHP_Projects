b79dc1467e41889e9fd98c25c2b05932bb05cdab
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2022-06-20 22:54:58 -0400

    Update MadLibsGame.php

--- PHP_tutorial/MadLibsGame.php
+++ PHP_tutorial/MadLibsGame.php
@@ -1,10 +1,23 @@
 <html>
     <body>
+        <form action="madlibsgame.php" method="get">
+            Color: <input type = "text" name="color"> <br>
+            Plural Noun: <input type = "text" name="pluralNoun"> <br>
+            Celebrity: <input type = "text" name="celebrity"> <br>
+            <input type="submit">
+
+        </form>
+        
+
 
         <?php
-            echo "Roses are red <br>";
-            echo "Violets are blue <br>";
-            echo "I love you <br>";
+            $color = $_GET["color"];
+            $pluralNoun = $_GET["pluralNoun"];
+            $celebrity = $_GET["celebrity"];
+
+            echo "Roses are $color <br>";
+            echo "$pluralNoun are blue <br>";
+            echo "I love you $celebrity <br>";
         ?>
 
     </body>
